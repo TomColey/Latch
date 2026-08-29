00e60639129894f06595bc6e4c0f69fe3b11bdae
TomColey <167567421+TomColey@users.noreply.github.com>
2026-08-29 19:57:23 +0100

    Fix KeyboardOptions import

--- app/src/main/kotlin/com/nathanb/lock/ui/screens/modes/NewModeScreen.kt
+++ app/src/main/kotlin/com/nathanb/lock/ui/screens/modes/NewModeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
@@ -61,7 +62,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
