cf9f9469d76767478307b62a6dd4513ecaad90f0
TomColey <167567421+TomColey@users.noreply.github.com>
2026-08-29 20:21:26 +0100

    Bump dev build to 0.1.0-dev.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.tomcoley.latch"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1015
-        versionName = "0.1.0-dev.15"
+        versionCode = 1016
+        versionName = "0.1.0-dev.16"
     }
 
     signingConfigs {
