92bad2460ebe43f662e517492543e8bf86749e84
TomColey <167567421+TomColey@users.noreply.github.com>
2026-08-29 18:32:52 +0100

    Bump Latch dev build to 0.1.0-dev.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.tomcoley.latch"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1008
-        versionName = "0.1.0-dev.8"
+        versionCode = 1009
+        versionName = "0.1.0-dev.9"
     }
 
     signingConfigs {
